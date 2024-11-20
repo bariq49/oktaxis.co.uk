5f0cda8673e8b11fd44cefc272110f21bfa86e9c
almaskhan1286 <muhammadalmas1286@gmail.com>
2024-11-20 13:15:16 +0500

    solve typescript error

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 "use client"
-import HomePage from "./(marketing)/home/page"
 
 import BookingForm from "@/components/BookingForm/BookingForm"
 
 export default function Home() {
     return(
         <div>
-            {/* <Home/> */}
+      
             <BookingForm/>
             
 
