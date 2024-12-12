1c152860c6f59ae6ad0d1b2f1fd61549036594c6
almaskhan1286 <muhammadalmas1286@gmail.com>
2024-12-13 02:09:20 +0500

    minor error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Apply CSP to all routes
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' https://js.stripe.com;", // Add other policies as needed
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
