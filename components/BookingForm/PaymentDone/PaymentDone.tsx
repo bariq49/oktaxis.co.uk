3380dd6334b23fc3c9f021f97a8e73870a1fc373
mussaddiqmahmood7 <mussaddiqmahmood7@gmail.com>
2025-01-14 16:22:40 +0500

    payment fractional updated

--- components/BookingForm/PaymentDone/PaymentDone.tsx
+++ components/BookingForm/PaymentDone/PaymentDone.tsx
@@ -2,7 +2,7 @@ import { StatusCard } from '@/components/Sections/StatusCard';
 import { useSearchParams } from 'next/navigation';
 import React, { SetStateAction, useEffect } from 'react'
 
-function PaymentDone({ setPaymentDone }: { setPaymentDone: React.Dispatch<SetStateAction<boolean>> }) {
+function PaymentDone({ setPaymentDone, paymentDone }: { setPaymentDone: React.Dispatch<SetStateAction<boolean>>, paymentDone:boolean }) {
     const search = useSearchParams()
     const paymentId = search.get("paymentId")
 
@@ -13,7 +13,7 @@ function PaymentDone({ setPaymentDone }: { setPaymentDone: React.Dispatch<SetSta
         }
     }, [paymentId])
     return (
-        <div className="absolute w-full h-full z-50 ">
+        <div className={`absolute w-full h-full z-50 ${paymentDone ? "visible" : "hidden"} `}>
             <div className="w-full h-full bg-black/30 flex items-center justify-center transition-all duration-300 relative">
 
                 <StatusCard type="success" onClose={() => { setPaymentDone(false) }} />
